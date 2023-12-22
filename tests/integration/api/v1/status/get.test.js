3f29bd7c5d990cdbc89c8fa1a4f6540a8c13647a
ravihlb <ravi@deliverydireto.com.br>
2023-12-22 08:21:30 -0300

    feat(api): finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const postgresVersion = responseBody.dependencies.database.version;
+  expect(postgresVersion).toBeDefined();
+  expect(postgresVersion).toEqual("16.1");
+
+  const databaseMaxConnections =
+    responseBody.dependencies.database.max_connections;
+  expect(databaseMaxConnections).toEqual(100);
+
+  const databaseOpenedConnections =
+    responseBody.dependencies.database.opened_connections;
+  expect(databaseOpenedConnections).toBeLessThanOrEqual(databaseMaxConnections);
+  expect(databaseOpenedConnections).toEqual(1);
 });
