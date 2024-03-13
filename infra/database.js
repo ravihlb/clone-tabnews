517f2cc37163daf3c7db1358f0c3e4cd2b8af4e5
ravihlb <ravi@deliverydireto.com.br>
2024-03-13 09:11:26 -0300

    feat(infra/database): add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -28,6 +20,16 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    }
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true
 }
 
 export default { query };
